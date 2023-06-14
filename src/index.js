e9e3d9f31670be0c3ba8a036b6971ca3f424a8fe
TifenSeng <zencrosia@gmail.com>
2023-06-14 13:14:39 +0700

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 const category = []
+const order = []
+const payment = []
+const invoice = []
+const rating = []
+const recommendation = []
 
-module.exports = category
+module.exports = {
+  category,
+  order,
+  payment,
+  invoice,
+  rating,
+  recommendation
+}
--- src/routes.js
+++ src/routes.js
@@ -2,29 +2,137 @@ const {
   addCategoryHandler,
   deleteCategoryHandler,
   updateCategoryHandler,
-  getCategoryHandler
+  getCategoryHandler,
+  addOrderHandler,
+  getOrderHandler,
+  addPaymentHandler,
+  getPaymentHandler,
+  updatePaymentHandler,
+  deletePaymentHandler,
+  addInvoiceHandler,
+  getInvoiceHandler,
+  updateInvoiceHandler,
+  deleteInvoiceHandler,
+  getRatingHandler,
+  addRatingHandler,
+  updateRatingHandler,
+  deleteRatingHandler,
+  getRecommendationHandler,
+  addRecommendationHandler,
+  updateRecommendationHandler,
+  deleteRecommendationHandler
 } = require('./handler')
 
 const routes = [
   {
     method: 'POST',
-    path: '/categories',
+    path: '/category',
     handler: addCategoryHandler
   },
   {
     method: 'PUT',
-    path: '/categories/{id}',
+    path: '/category/{id}',
     handler: updateCategoryHandler
   },
   {
     method: 'DELETE',
-    path: '/categories/{id}',
+    path: '/category/{id}',
     handler: deleteCategoryHandler
   },
   {
     method: 'GET',
-    path: '/categories',
+    path: '/category',
     handler: getCategoryHandler
+  },
+  {
+    method: 'POST',
+    path: '/order',
+    handler: addOrderHandler
+  },
+  {
+    method: 'GET',
+    path: '/order',
+    handler: getOrderHandler
+  },
+  {
+    method: 'POST',
+    path: '/payment',
+    handler: addPaymentHandler
+  },
+  {
+    method: 'GET',
+    path: '/payment',
+    handler: getPaymentHandler
+  },
+  {
+    method: 'PUT',
+    path: '/payment/{id}',
+    handler: updatePaymentHandler
+  },
+  {
+    method: 'DELETE',
+    path: '/payment/{id}',
+    handler: deletePaymentHandler
+  },
+  {
+    method: 'POST',
+    path: '/invoice',
+    handler: addInvoiceHandler
+  },
+  {
+    method: 'GET',
+    path: '/invoice',
+    handler: getInvoiceHandler
+  },
+  {
+    method: 'PUT',
+    path: '/invoice/{id}',
+    handler: updateInvoiceHandler
+  },
+  {
+    method: 'DELETE',
+    path: '/invoice/{id}',
+    handler: deleteInvoiceHandler
+  },
+  {
+    method: 'GET',
+    path: '/rating',
+    handler: getRatingHandler
+  },
+  {
+    method: 'POST',
+    path: '/rating',
+    handler: addRatingHandler
+  },
+  {
+    method: 'PUT',
+    path: '/rating/{id}',
+    handler: updateRatingHandler
+  },
+  {
+    method: 'DELETE',
+    path: '/rating/{id}',
+    handler: deleteRatingHandler
+  },
+  {
+    method: 'GET',
+    path: '/recommendation',
+    handler: getRecommendationHandler
+  },
+  {
+    method: 'POST',
+    path: '/recommendation',
+    handler: addRecommendationHandler
+  },
+  {
+    method: 'PUT',
+    path: '/recommendation/{id}',
+    handler: updateRecommendationHandler
+  },
+  {
+    method: 'DELETE',
+    path: '/recommendation/{id}',
+    handler: deleteRecommendationHandler
   }
 ]
 
